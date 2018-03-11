fd3309175ab633e5d12fd2e0ec7dcbf748ed9848
sktong <35784341+sktong@users.noreply.github.com>
2018-03-11 18:47:57 -0400

    Update Plot 3.R

--- Plot 3.R
+++ Plot 3.R
@@ -1,9 +1,9 @@
 powerDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
 
-# Making a POSIXct date capable of being filtered and graphed by time of day
+
 powerDT[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
 
-# Filter Dates for 2007-02-01 and 2007-02-02
+
 powerDT <- powerDT[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
 
 png("plot3.png", width=480, height=480)
